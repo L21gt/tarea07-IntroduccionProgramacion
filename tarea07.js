c856f14ac60d439b3b9526c61e2ca53e3ee9f91e
Luis Velasquez <luis_777521@hotmail.com>
2024-06-24 21:40:50 -0600

    adding problema III and IV

--- tarea07.js
+++ tarea07.js
@@ -101,3 +101,110 @@ console.log("*************** Problema II ***************");
 console.log(mensaje); 
 console.log(" ");
 console.log("==========================================================");
+
+
+
+//-----------------------------------------------------------------------------------
+/**
+ * Problema III ( 10 Puntos)
+Crea dos objetos, `persona1` y `persona2`, cada uno con una propiedad
+`nombre`. Define una función `saludar` que devuelva un mensaje de
+saludo utilizando la propiedad `nombre`. Usa `call()` para invocar la
+función `saludar` con el contexto de `persona2`.
+// Objeto persona1
+const xxxxxxxx = {
+xxxxxx: 'Juan'
+};
+// Objeto persona2
+const xxxxxxxx= {
+xxxxxx: 'María'
+};
+// Función saludar
+function xxxxxxx() {
+return `Hola, mi nombre es ${this.nombre}.`;
+}
+// Usar call() para invocar saludar con el contexto de persona2
+const saludo = saludar.xxxx(persona2);
+// Imprimir el saludo
+console.log(saludo); // Debe imprimir: Hola, mi nombre es María.
+ */
+
+// creacion de objeto 'persona1'
+const persona1 = {
+    nombre:'Juan',
+};
+
+// creacion de objeto 'persona2'
+const persona2 = {
+    nombre: 'Maria'
+};
+
+// creacion de funcion saludar
+function saludar () {
+    return `Hola, mi nombre es ${this.nombre}`;
+}
+
+// Usar call() para invocar saludar con el contexto de persona2
+const saludo1 = saludar.call(persona2);
+
+
+// Imprimir el área del cuadrado
+console.log("*************** Problema III ***************");
+console.log(saludo1);
+console.log(" ");
+console.log("==========================================================");
+
+//-----------------------------------------------------------------------------------
+
+/**
+ * Problema IV ( 10 Puntos)
+Crea un objeto `rectangulo` con propiedades `ancho` y `alto`, y un
+método `area` que calcule el área del rectángulo. Luego, crea un objeto
+`cuadrado` con una propiedad `lado`. Usa `call()` para invocar el método
+`area` del objeto `rectangulo` con el contexto del objeto `cuadrado`.
+
+// Objeto rectangulo
+const xxxxxxxxxx = {
+xxxxx: 0,
+xxxx: 0,
+xxxx: function() {
+return this.ancho * this.alto;
+}
+};
+
+// Objeto cuadrado
+const xxxxxxxx = {
+xxxx: 5
+};
+
+// Usar call() para invocar area con el contexto de cuadrado
+const areaCuadrado = rectangulo.area.call({ ancho: cuadrado.lado, alto:
+cuadrado.lado });
+
+// Imprimir el área del cuadrado
+console.log(areaCuadrado); // Debe imprimir: 25
+ */
+
+//creacion de objeto 'rectangulo'
+const rectangulo = {
+    ancho: 0,
+    alto: 0,
+    area: function () {
+        return this.ancho*this.alto;
+    }
+};
+
+// creacion de objeto 'cuadrado'
+const cuadrado = {
+    lado: 5,
+}
+
+// Usar call() para invocar area con el contexto de cuadrado
+const areaCuadrado = rectangulo.area.call({ancho: cuadrado.lado, alto: cuadrado.lado});
+
+// Imprimir el área del cuadrado
+console.log("*************** Problema IV ***************");
+console.log(areaCuadrado);
+console.log(" ");
+console.log("==========================================================");
+
